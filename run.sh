20514429ece29a6c6ebd22b7e9ba12e9583607ca
Jianyong Wu <616191051@qq.com>
2018-09-21 23:21:16 +0800

    Add Makefile in root director

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 set -e
-make -C boot/
-make -C init/ kernel.bin
+
+#build all
+make
+
+#install bootloader and kernel into hard disk
 dd if=boot/boot.bin of=c.img bs=512 count=1 conv=notrunc
 dd if=boot/setup.bin of=c.img seek=1 conv=notrunc
 dd if=init/kernel.bin of=c.img seek=9 conv=notrunc
+
+#run kernel in qemu and bochs
 qemu-system-x86_64 c.img -m 1024
 #bochs -f bochsrc.bxrc
