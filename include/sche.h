773c39b10a21108ca3ddb8785d13e65e29a13ee6
Jianyong Wu <616191051@qq.com>
2018-11-25 12:01:35 +0800

    schedule in progress

--- include/sche.h
+++ include/sche.h
@@ -1,12 +1,43 @@
 #ifndef HS_SCHE
 #define HS_SCHE
+#include "global.h"
+
+#define PAGE_SIZE 1024
 
 long long heartbeat;
-void scheduler();
 typedef void thread_func(void*);
-void thread_create(struct task_struct* pthread, thread_func func, void *arg);
-void init_thread(struct task_struct* pthread, int prio);
-struct task_struct* thread_start(thread_func func, void *arg);
+
+struct thread_stack{
+        u32 ebp;
+        u32 ebx;
+        u32 ebi;
+        u32 esi;
+        void (*eip)(thread_func* func, void* func_arg);
+        thread_func *func;
+        void *func_arg;
+};
+
+enum task_status {
+        RUNNING,
+        READY,
+        BLOCKED,
+        WAITING,
+        DIED
+};
+
+struct task {
+        struct thread_stack *thread_stack;
+        enum task_status status;
+        u32 prio;
+        u32 ticks;
+        u32* pde;
+        u32 magic;
+};
+
+void scheduler();
+void thread_create(struct task *thread, thread_func function, void *arg);
+void thread_start(thread_func function, void *arg);
+void init_thread(struct task *thread, int proi);
 
 
 
--- kernel/sche.c
+++ kernel/sche.c
@@ -4,48 +4,56 @@
 #include "global.h"
 void dispA();
 void dispB();
-struct thread_stack{
-	u32 ebp;
-	u32 ebx;
-	u32 ebi;
-	u32 esi;
-	void (*eip)(thread_func* func, void* func_arg);
-};
 
-enum task_status {
-	RUNNING,
-	READY,
-	BLOCKED,
-	WAITING,
-	DIED
-};
-	
-struct task {
-	void *thread_stack;
-	enum task_status status;
-	u32 prio;
-	u32 ticks;
-	u32* pde;
-	u32 magic;
-};
-
-struct task task_list[20];
+struct task *task_list[20];
 u32 task_number=0, task_max=0;
+void thread_function(thread_func function, void *arg)
+{
+        function(arg);
+}
 
 void scheduler()
 {
-	struct task cur, next;
-	if(task_max < 1)
+	struct task *cur,*next;
+	if(task_max < 2)
 		return;
 	if(task_number < task_max)
 	{
 		cur = task_list[task_number % task_max];
 		next = task_list[(task_number + 1) % task_max];
 		task_number++;
-//		switch_to(cur, next);
+		task_number %= task_max;
+		cur->status = READY;
+		switch_to(cur, next);
 	}
 	
 }	
+
+void init_thread(struct task *thread, int proi)
+{
+	thread->status = RUNNING;
+	thread->thread_stack = thread + PAGE_SIZE;
+}
+
+void thread_create(struct task *thread, thread_func function, void *arg)
+{
+	thread->thread_stack -= sizeof(thread);
+	thread->thread_stack->eip = thread_function;
+	thread->thread_stack->func = function;
+	thread->thread_stack->func_arg = arg;
+	thread->thread_stack->ebp = 0;
+	thread->thread_stack->ebx = 0;
+	thread->thread_stack->ebi = 0;
+	thread->thread_stack->esi = 0;
+}
+
+void thread_start(thread_func function, void *arg)
+{
+	struct task *thread = get_kernel_page();
+	thread_init(thread);
+	thread_create(thread, function, arg);
+	task_list[task_max++] = thread;
+}
 
 void dispA()
 {
