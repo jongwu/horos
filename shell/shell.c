7c1e5907da7134f1fb19d07dcf4df29fb74a1e6e
Jianyong Wu <616191051@qq.com>
2019-01-01 22:30:30 +0800

    add shell to horos

---
+++ shell/shell.c
@@ -0,0 +1,71 @@
+#include "lib.h"
+int cmd_open;
+void *cmd_arg_table[10];
+void do_shell()
+{
+	disp_str("welcomm to horos shell\n");
+	disp_str("command list:\n$ ");
+	for(int i = 0; i < CMD_NUM; i++)
+	{
+		disp_str(cmd_table[i]);
+		disp_str(", ");
+	}
+	cmd_open = 1;
+	int k = 0;
+//	disp_str(kd_buf);
+	disp_str("\n$");
+	while(1)
+	{
+		if(p_kd_buf < 1)continue;
+		if(kd_buf[p_kd_buf - 1] == '\n')
+		{	
+			char *arg, *cmd;
+			int i;
+			k = 0;
+			kd_buf[p_kd_buf - 1] = '\0';
+			parse_cmd(kd_buf, cmd, arg);
+			for(i =0 ; i<CMD_NUM; i++)
+			{
+				if(str_equal(cmd_table[i], cmd))
+				{
+					thread_function(cmd_fun_table[i], arg);
+					p_kd_buf = 0;
+					disp_str("\n$");
+					break;
+				}
+			}
+			p_kd_buf = 0;
+			if(i < CMD_NUM)continue;
+			shell_disp(cmd);
+			disp_str(": command not found \n$");
+		}
+	}
+}
+
+int parse_cmd(char *buf, char *cmd, char *arg)
+{
+	int i = 0;
+	while(i < p_kd_buf && buf[i] && buf[i] != ' ')
+	{
+		*(cmd +i) = *(buf + i);
+		i++;
+	}
+	*(cmd +i) = '\0';
+	if(buf[i] != ' '){
+//	disp_int(p_kd_buf);
+	return 0;}
+	int j = 0;
+	i++;
+	while(i < p_kd_buf)
+	{
+		*(arg + j) = buf[i];
+		i++; j++;
+	}
+	return 1;
+}
+
+void shell_disp(char *str)
+{
+	disp_str("$");
+	disp_str(str);
+}
